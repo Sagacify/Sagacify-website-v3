347a5f1b78153daa7954ffbe7d817f988d83a536
elisagilis <elisa.gilis@hotmail.fr>
2015-04-27 17:33:48 +0200

    Header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	
+	// Navigation toggle
+	$('body').on('click', '.header--navbutton--rdicon', function(event) {
+		event.preventDefault();
+		$(this).toggleClass('active');
+		$('.navigation').toggleClass('active');
+	});
+
+
 });
