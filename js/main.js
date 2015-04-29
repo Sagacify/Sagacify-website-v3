7eb1d68b569d96532be93860db463495840acd5e
elisagilis <elisa.gilis@hotmail.fr>
2015-04-29 16:58:37 +0200

    d

--- js/main.js
+++ js/main.js
@@ -8,4 +8,10 @@ $(document).ready(function () {
 	});
 
 	//sagacify.7274f0f9
+
+	// Provide your access token
+	L.mapbox.accessToken = 'pk.eyJ1Ijoic2FnYWNpZnkiLCJhIjoiSEhlc0Y3OCJ9.oK-VhYtBUvM3zoVMJXekxg';
+	// Create a map in the div #map
+	L.mapbox.map('map', 'sagacify.7274f0f9')
+	.setView([50.826609, 4.399905999999987], 16);
 });
