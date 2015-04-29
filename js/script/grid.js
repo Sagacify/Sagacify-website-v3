9c9b3796a659213c8fd5f36a433e07b62b8bcd04
Julien He <hoshi.graphi@gmail.com>
2015-04-29 15:15:07 +0200

    d

---
+++ js/script/grid.js
@@ -0,0 +1,54 @@
+var widthDocument = document.querySelector(".header").offsetWidth;
+gutterSize = widthDocument / 40;
+window.onload = function() {
+	matrix();
+};
+
+function getHeight(argument) {
+	var heightCount = 0,
+	args = argument.querySelectorAll('.data--count');
+	Array.prototype.forEach.call(args, function(el, i){
+		heightCount += parseInt(el.offsetHeight);
+	});
+	return heightCount + gutterSize + "px";
+}
+
+function matrix(){
+	//Move each .block--element on the good position and give good width
+	var elem = document.querySelectorAll('.blocks--element');
+	Array.prototype.forEach.call(elem, function(el, i){
+		el.style.left = el.dataset.left * gutterSize + "px";
+		el.style.width = el.dataset.width * gutterSize + "px";
+	});
+
+	//Get all .blocks
+	var element = document.querySelectorAll('.blocks');
+	Array.prototype.forEach.call(element, function(el, i){
+
+		//Give the block a height and margin-bottom
+		el.style.height = getHeight(el);
+		el.style.marginBottom = gutterSize + "px";
+
+		//Reset the previous height for left and right
+		var heighetElementLeft = 0,
+		heighetElementRight = 0;
+
+		//Move element on top for left and right
+		var dataLeft = el.querySelectorAll('.data-left');
+		Array.prototype.forEach.call(dataLeft, function(el, i){
+			if(el.hasAttribute('data-reset')){
+				heighetElementLeft = 0;
+			}
+			el.style.top = heighetElementLeft + ( el.dataset.top * gutterSize) + "px";
+			heighetElementLeft += el.offsetHeight;
+		});
+
+		var dataRight = el.querySelectorAll('.data-right');
+		Array.prototype.forEach.call(dataRight, function(el, i){
+			el.style.top = heighetElementRight + ( el.dataset.top * gutterSize) + "px";
+			heighetElementRight += el.offsetHeight;
+		});
+	});
+
+	new WOW().init();
+}
