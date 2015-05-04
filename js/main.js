0a3b14423f75773b3ab662d712f5cca5b83c6ad0
elisagilis <elisa.gilis@hotmail.fr>
2015-05-04 11:11:33 +0200

    team

--- js/main.js
+++ js/main.js
@@ -1,12 +1,40 @@
 $(document).ready(function () {
 	
 	// Navigation toggle
-	$('body').on('click', '.header--navbutton--rdicon', function(event) {
-		event.preventDefault();
-		$(this).toggleClass('active');
-		$('.navigation').toggleClass('active');
-	});
+	var elements = document.querySelector('.header--navbutton--rdicon');
+	var navigation = document.querySelector('.navigation');
+	elements.addEventListener("click", function(event){
+		event.preventDefault;
+		var el = elements;
+		if (el.classList) {
+			el.classList.toggle('active');
+		} else {
+			var classes = el.className.split(' ');
+			var existingIndex = classes.indexOf('active');
 
+			if (existingIndex >= 0)
+				classes.splice(existingIndex, 1);
+			else
+				classes.push('active');
+
+			el.className = classes.join(' ');
+		}
+
+		el = navigation;
+		if (el.classList) {
+			el.classList.toggle('active');
+		} else {
+			var classes = el.className.split(' ');
+			var existingIndex = classes.indexOf('active');
+
+			if (existingIndex >= 0)
+				classes.splice(existingIndex, 1);
+			else
+				classes.push('active');
+
+			el.className = classes.join(' ');
+		}
+	});
 });
 
 window.onload=function(){
@@ -28,10 +56,17 @@ window.onload=function(){
 		new WOW().init();
 	}
 
-	if($('#map').length > 0){
-		// Provide your access token
+	// if($('#map').length > 0){
+	// 	// Provide your access token
+	// 	L.mapbox.accessToken = 'pk.eyJ1Ijoic2FnYWNpZnkiLCJhIjoiSEhlc0Y3OCJ9.oK-VhYtBUvM3zoVMJXekxg';
+ //    	// Create a map in the div #map
+ //    	L.mapbox.map('map', 'sagacify.7274f0f9').setView([50.826609, 4.399905999999987], 16);
+ //    }
+
+    if(('#map').length > 0) {
+    	// Provide your access token
 		L.mapbox.accessToken = 'pk.eyJ1Ijoic2FnYWNpZnkiLCJhIjoiSEhlc0Y3OCJ9.oK-VhYtBUvM3zoVMJXekxg';
     	// Create a map in the div #map
-    	L.mapbox.map('map', 'sagacify.7274f0f9').setView([50.826609, 4.399905999999987], 16);
+    	L.mapbox.map('map', 'sagacify.7274f0f9').setView([50.826609, 4.399905999999987], 16);	
     }
 };
