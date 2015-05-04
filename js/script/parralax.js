3cca2838a9f5cf637ee9937ad1ff5130edb71133
Julien He <hoshi.graphi@gmail.com>
2015-05-04 15:47:30 +0200

    d

--- js/script/parralax.js
+++ js/script/parralax.js
@@ -11,10 +11,13 @@ window.onscroll=function(){
 	//Get all element for the parralax effect
 	var elemPara = document.querySelectorAll('[data-parralax]');
 	Array.prototype.forEach.call(elemPara, function(el, i){
-		var bottom_of_object = el.offsetTop + el.offsetHeight;
-		var bottom_of_window = window.pageYOffset + window.innerHeight + 150;
-		var speedi = ( el.dataset.parralax != 0 ) ? el.dataset.parralax : 1;
+		var bottom_of_object = el.offsetTop + el.offsetHeight,
+		bottom_of_window = window.pageYOffset + window.innerHeight,
+		speedi = ( el.dataset.parralax != 0 ) ? el.dataset.parralax : 1;
 		if( bottom_of_window > bottom_of_object ){
+			if(el.classList.contains('data-indexif')){
+				console.log(el);
+			}
 			el.style.transform =
 			"translate3d(0, " + scrollPercentage * speedi + "px, 0)";
 		}
