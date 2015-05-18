8bec69f3211eb6e06275f380fb2176d82a552d23
Julien He <hoshi.graphi@gmail.com>
2015-05-18 11:00:24 +0200

    d

--- js/main.js
+++ js/main.js
@@ -31,6 +31,21 @@ $(document).ready(function () {
 			el.className = classes.join(' ');
 		}
 	});
+
+	var $body    = $('html, body'),
+	content  = $('#smooth').smoothState({
+		prefetch: true,
+		pageCacheSize: 4,
+		onStart: {
+			duration: 250,
+			render: function (url, $container) {
+				content.toggleAnimationClass('is-exiting');
+				$body.animate({
+					scrollTop: 0
+				});
+			}
+		}
+	}).data('smoothState');
 });
 
 window.onload=function(){
